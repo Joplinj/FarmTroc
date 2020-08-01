02b0994224639463366e4f2dbc367d8881a0210f
Rémi Chassereaux <chassereauxremi@gmail.com>
2020-08-01 11:33:54 +0200

    assets

---
+++ assets/header.php
@@ -0,0 +1,1 @@
+<div>moi je suis le header</div>
--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-coucou 2
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FarmTroc</title>
+</head>
+
+<body>
+    <?php
+     require "./assets/header.php"
+    ?>
+
+</body>
+
+</html>
