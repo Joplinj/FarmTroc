833a07382193fbf8739bcc7e144c16b7538ad29b
Rémi Chassereaux <chassereauxremi@gmail.com>
2020-08-01 11:47:25 +0200

    header

--- assets/header.php
+++ assets/header.php
@@ -1,1 +1,20 @@
-<div>moi je suis le header</div>
+<header>
+    <div class="container">
+        <div class="headerBar">
+            <div class="logoContainer">
+                <p>Mon logo</p>
+            </div>
+            <div class="menuContainer">
+                <div class="menu">
+                    <ul>
+                        <li>Accueil</li>
+                        <li>Troc</li>
+                        <li>Carte</li>
+                        <li>A propos</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
